7b0b5e989a7bebfa6f74916f817b306cfc774485
AnanthaLakshmiKannan <ananthalakshmi.k@syncfusion.com>
2023-12-07 20:26:56 +0530

    Added sample for customizing-popup-view-appearance-using-styles-in-.net-maui-popup

---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Syncfusion.Maui.Popup;
+namespace MauiPopup;
+using Syncfusion.Maui.Popup;
+using Syncfusion.Maui.Core;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+		InitializeComponent();
+    }
+}
+
