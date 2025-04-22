9e466e601f20c2c57f7eab36ae7963a4e95f1866
SowntharyaJ <sowntharya.jayamoorthy@syncfusion.com>
2025-04-22 11:15:21 +0530

    Alignment issue resolved

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,11 +5,10 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-    }
+	}
 
 	protected override Window CreateWindow(IActivationState activationState)
 	{
 		return new Window(new MainPage());
 	}
-
 }
